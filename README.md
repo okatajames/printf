761c4b83072bd10083a90fd499638759d75ce3d6
okatajames <jamesokata3@gmail.com>
2023-03-27 04:34:40 +0300

    0x11 C - printf

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # printf
-Group repository for the alx printf project
+Group repository for the alx printf project between me and my partner Aggrey
---
+++ flags.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_flags - calculates the active flags
+ * By okatajames and aggreyjr
+ * @format: formatted string in which to print the params
+ * @i: take a parameter
+ * Return: flags
+ */
+
+int get_flags(const char *format, int *i)
+{
+    /* - + 0 # ' ' */
+    /* 1 2 4 8 16 */
+    int j, curr_i;
+    int flags = 0;
+    const char FLAGS_CH[] = {-,+, }
+}
+
+
+
+
+
+
